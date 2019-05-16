caeea04350f5a4216d753db763c4a53f17706956
dbucce <dbucce@hotmail.com>
2019-05-15 20:22:21 -0600

    byte_striping y demux

--- Documents/digitales_2/proyecto/bs.tcl
+++
@@ -1,9 +0,0 @@
-read_verilog byte_striping.v
-hierarchy -check -top byte_striping
-proc; opt; fsm; opt; memory; opt
-techmap; opt
-dfflibmap -liberty cmos_cells.lib
-abc -liberty cmos_cells.lib
-clean
-write_verilog byte_striping_synth.v
-show byte_striping
